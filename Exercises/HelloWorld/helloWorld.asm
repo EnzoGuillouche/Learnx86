9e73f82b054ad586f47475a0b3c8b5c50c1483d0
EnzoGuillouche <145991192+EnzoGuillouche@users.noreply.github.com>
2025-04-30 14:00:43 +0200

    Added dynamic mac build file & minor updates

--- Exercises/HelloWorld/helloWorld.asm
+++ Exercises/HelloWorld/helloWorld.asm
@@ -1,1 +1,6 @@
 ; Create a program that prints "Hello, World!" on the DOSBox console.
+; int 21h to print a string on the console
+;   -> ah = 40h
+;   -> bx = 1 as stdout
+;   -> dx = string 
+;   -> cx = string's number of bytes
